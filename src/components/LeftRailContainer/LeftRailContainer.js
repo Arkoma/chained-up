e1cdfbe04f76ae09b5ed420fc06d19667c716247
Arkoma <aaron@adburk.net>
2018-02-07 14:18:00 -0600

    add left rail component
    - some path typo is going on... it's not compiling.
    - error message says cant find module

---
+++ src/components/LeftRailContainer/LeftRailContainer.js
@@ -0,0 +1,13 @@
+import React, { Component} from 'react';
+
+class LeftRailContainer extends Component {
+    render() {
+      return (
+        <div className="LeftRailContainer">
+          <h3>336</h3>
+        </div>
+      );
+    }
+  }
+  
+export default LeftRailContainer;
